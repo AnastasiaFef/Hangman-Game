29d856aed38498c4568abd4d8bc93bbd98246784
AnastasiaFef <fefilovaanastasiya@gmail.com>
2017-11-21 15:19:54 -0800

    Fixed a bug where last letter from previous word would save to a new array, so same letter couldn't been used in a new word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -18,8 +18,6 @@ var compChoice ="";
 var albpabet=['a','b','c','d','e','f','g','h','i','j','k',
 'l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
-
-
 //the function that selects word ramdomly from states array
 function chooseWord(){ 
 	compChoice = states[Math.floor(Math.random()*states.length)];
@@ -31,10 +29,6 @@ function chooseWord(){
 	// blankWord();
 	compChoiceArray = compChoice.split(" ");
 	console.log("compChoiceArray after restart: "+ compChoiceArray)
-// 	}
-
-// //show underscores instead of letters in words
-// function blankWord(){
 	console.log("Array: "+ compChoiceArray);
 	function first(){
 		var blWord = "";
@@ -44,7 +38,6 @@ function chooseWord(){
 		console.log(""+blWord)
 	return blWord;
 	}
-	// var blankW= [];
 	blankW.push(first());
 	if (compChoiceArray.length == 2){
 		var blWord2 = "";
@@ -55,26 +48,9 @@ function chooseWord(){
 	}
 
 	console.log("blankW: " + blankW);
-// }
-
-// //display (append) the blankW in DOM
-// function appendWord(){
-	// var wordContainer= document.createElement("div")
-	// wordContainer.setAttribute("id","compWordOne");
-	// console.log("wordContainer1: ");
-	// console.log(wordContainer);
-	// var text = document.createTextNode(blankW[0]);
-	// wordContainer.appendChild(text);
-	// console.log(text)
 	document.getElementById("compWordOne").innerText = blankW[0];
 
 	if (blankW.length === 2){
-		// var wordContainer= document.createElement("div")
-		// wordContainer.setAttribute("id","compWordTwo");
-		// console.log("wordContainer2: ");
-		// console.log(wordContainer)
-		// var text = document.createTextNode(blankW[1]);
-		// wordContainer.appendChild(text);
 		document.getElementById("compWordTwo").innerText = blankW[1];
 	}
 	else{
@@ -100,168 +76,132 @@ document.onkeyup = function(event){
 		}
 		console.log("letterAlreadyUsed after loop: " + letterAlreadyUsed)
 		if (letterAlreadyUsed === 0){
-			firstTimeLetter(userGuess);
+			var met=false;
+			console.log("//////////	function firstTimeLetter()")
+			console.log("compChoiceArray[0].length: "+compChoiceArray[0].length)
+			// if guessed letter is in 1st part of compChoice, replace _ with that letter and save in blankW[0]
+			for (var i=0; i<compChoiceArray[0].length; i++){
+				if (compChoiceArray[0].substr(i, 1) === userGuess){	//substring func to replace arrayter
+					console.log("before replacement "+blankW);
+					// blankW=blankW[0].replaceAt(i,userGuess);
+					blankW[0] = blankW[0].substr(0, i) + userGuess.toUpperCase() + blankW[0].substr(i + 1);
+					console.log("after replacenent: "+blankW);
+					met=true;
+				}
+			}
+			// if guessed letter is in 2nd part of compChoice, replace _ with that letter and save in blankW[1]
+			if (compChoiceArray.length === 2){
+				for (var i=0; i<compChoiceArray[1].length; i++){
+					if (compChoiceArray[1].substr(i, 1) === userGuess){
+						console.log("before replacement of 2 "+blankW);
+						// blankW=blankW[0].replaceAt(i,userGuess);
+						blankW[1] = blankW[1].substr(0, i) + userGuess.toUpperCase() + blankW[1].substr(i + 1);
+						console.log("after replacenent: of 2 "+blankW);
+						met=true;
+					}
+				}
+			}
+			document.getElementById("compWordOne").innerText= blankW[0];
+			if (compChoiceArray.length === 2){
+				document.getElementById("compWordTwo").innerText= blankW[1];
+			}
+			if (met===false){
+				guessesRemaining -=1;
+				document.getElementById("guesses").innerText="Number of guesses remaining: "+guessesRemaining ;
+
+
+				console.log("wrongGuessed: " +wrongGuessed + typeof wrongGuessed)
+				console.log("clickedLetters: "+clickedLetters + typeof clickedLetters)
+
+				wrongGuessed.push(userGuess);
+			
+				document.getElementById("usedLetters").innerText="Letters already guessed: "+wrongGuessed ;
+				console.log(blankW)
+				if (guessesRemaining === 0){
+					console.log("I AM IN lost func");
+					alert("Looser");
+					document.getElementById("wins").innerText= "Wins: "+ wins;
+					guessesRemaining=12;
+					document.getElementById("guesses").innerText="Number of guesses remaining: "+guessesRemaining;
+					document.getElementById("compWordOne").textContent = "";
+					document.getElementById("compWordTwo").innerText = "";
+					compChoice="";
+					blankW=[];
+					chooseWord();
+					console.log("compChoice "+compChoice);
+					clickedLetters = [];
+					return;
+				}
+			} 
+			else{
+				console.log("I AM IN isWordOpen func");
+				var isWOpen1=true;
+				var isWOpen2=true;
+				for (var b=0; b<blankW[0].length; b++){
+					if (blankW[0].substr(b, 1) === "-"){	
+						isWOpen1 = false;
+					}
+				}
+				if (blankW.length === 2){
+					for (var b=0; b<blankW[1].length; b++){
+						if (blankW[1].substr(b, 1) === "-"){
+							isWOpen1 = false;
+						}
+					}
+				}	
+				if (blankW.length === 2){
+					if (isWOpen1 && isWOpen2){
+						console.log("I AM IN wordOpen func");
+						wins += 1;
+						document.getElementById("wins").innerText= "Wins: "+ wins;
+						guessesRemaining=12;
+						document.getElementById("guesses").innerText="Number of guesses remaining: "+guessesRemaining;
+						document.getElementById("compWordOne").textContent = "";
+						document.getElementById("compWordTwo").innerText = "";
+						compChoice="";
+						blankW=[];
+						chooseWord();
+						console.log("compChoice "+compChoice);
+						clickedLetters = [];
+						return;
+					}
+					else{
+						return
+					}
+				}
+				else{
+					if (isWOpen1){
+						console.log("I AM IN wordOpen func");
+						wins += 1;
+						document.getElementById("wins").innerText= "Wins: "+ wins;
+						guessesRemaining=12;
+						document.getElementById("guesses").innerText="Number of guesses remaining: "+guessesRemaining;
+						document.getElementById("compWordOne").textContent = "";
+						document.getElementById("compWordTwo").innerText = "";
+						compChoice="";
+						blankW=[];
+						chooseWord();
+						console.log("compChoice "+compChoice);
+						clickedLetters = [];
+						return 
+					}
+					else{
+						return
+					}
+				}
+			}
+			console.log("blankW is " +blankW)
+			console.log("compChoiceArray "+ compChoiceArray)
 		}
 		clickedLetters.push(userGuess);
 		console.log("clickedLetters on ONKEYUP: "+clickedLetters + " ||| typeof:  " + typeof clickedLetters);
 	}
 }
 
-
-function firstTimeLetter(){
-	var met=false;
-	console.log("//////////	function firstTimeLetter()")
-	console.log("compChoiceArray[0].length: "+compChoiceArray[0].length)
-	// if guessed letter is in 1st part of compChoice, replace _ with that letter and save in blankW[0]
-	for (var i=0; i<compChoiceArray[0].length; i++){
-		if (compChoiceArray[0].substr(i, 1) === userGuess){	//substring func to replace arrayter
-			console.log("before replacement "+blankW);
-			// blankW=blankW[0].replaceAt(i,userGuess);
-			blankW[0] = blankW[0].substr(0, i) + userGuess.toUpperCase() + blankW[0].substr(i + 1);
-			console.log("after replacenent: "+blankW);
-			met=true;
-		}
-	}
-	// if guessed letter is in 2nd part of compChoice, replace _ with that letter and save in blankW[1]
-	if (compChoiceArray.length === 2){
-		for (var i=0; i<compChoiceArray[1].length; i++){
-			if (compChoiceArray[1].substr(i, 1) === userGuess){
-				console.log("before replacement of 2 "+blankW);
-				// blankW=blankW[0].replaceAt(i,userGuess);
-				blankW[1] = blankW[1].substr(0, i) + userGuess.toUpperCase() + blankW[1].substr(i + 1);
-				console.log("after replacenent: of 2 "+blankW);
-				met=true;
-			}
-		}
-	}
-	document.getElementById("compWordOne").innerText= blankW[0];
-	if (compChoiceArray.length === 2){
-		document.getElementById("compWordTwo").innerText= blankW[1];
-	}
-	if (met===false){
-		guesses()
-	} 
-	else{
-		isWordOpen();
-	}
-	console.log("blankW is " +blankW)
-	console.log("compChoiceArray "+ compChoiceArray)
-	return blankW;
-} 
-
-function isWordOpen(){
-	console.log("I AM IN isWordOpen func");
-	var isWOpen1=true;
-	var isWOpen2=true;
-	for (var b=0; b<blankW[0].length; b++){
-		if (blankW[0].substr(b, 1) === "-"){	
-			isWOpen1 = false;
-		}
-	}
-	if (blankW.length === 2){
-		for (var b=0; b<blankW[1].length; b++){
-			if (blankW[1].substr(b, 1) === "-"){
-				isWOpen1 = false;
-			}
-		}
-	}	
-	if (blankW.length === 2){
-		if (isWOpen1 && isWOpen2){
-			wordOpen();
-		}
-		else{
-			return
-		}
-	}
-	else{
-		if (isWOpen1){
-			wordOpen();
-		}
-		else{
-			return
-		}
-	}
-}
-
-function wordOpen(){
-	//change image
-	//play song
-	console.log("I AM IN wordOpen func");
-	wins += 1;
-	document.getElementById("wins").innerText= "Wins: "+ wins;
-	guessesRemaining=12;
-	document.getElementById("guesses").innerText="Number of guesses remaining: "+guessesRemaining;
-	document.getElementById("compWordOne").textContent = "";
-	document.getElementById("compWordTwo").innerText = "";
-	compChoice="";
-	blankW=[];
-	chooseWord();
-	console.log("compChoice "+compChoice);
-	clickedLetters = [];
-	// compChoiceArray = compChoice.split(" ");
-	// blankWord();
-	// appendWord();
-}
-
-function guesses(){
-		guessesRemaining -=1;
-		document.getElementById("guesses").innerText="Number of guesses remaining: "+guessesRemaining ;
-
-
-		console.log("wrongGuessed: " +wrongGuessed + typeof wrongGuessed)
-		console.log("clickedLetters: "+clickedLetters + typeof clickedLetters)
-
-		wrongGuessed.push(userGuess);
-	
-		document.getElementById("usedLetters").innerText="Letters already guessed: "+wrongGuessed ;
-		console.log(blankW)
-		if (guessesRemaining === 0){
-			lost();
-		}
-	}
-
-function lost(){
-	//change image
-	//play song
-	console.log("I AM IN lost func");
-	alert("Looser");
-	document.getElementById("wins").innerText= "Wins: "+ wins;
-	guessesRemaining=12;
-	document.getElementById("guesses").innerText="Number of guesses remaining: "+guessesRemaining;
-	document.getElementById("compWordOne").textContent = "";
-	document.getElementById("compWordTwo").innerText = "";
-	compChoice="";
-	blankW=[];
-	chooseWord();
-	console.log("compChoice "+compChoice);
-	clickedLetters = [];
-}
-
 var blankW=[];
+clickedLetters = [];
 blankW=chooseWord();
-// var compChoiceArray = compChoice.split(" ");
-// blankW = blankWord();
-// appendWord()
 
-
-
-// document.getElementByClassName("word").append
-// if (compChoiceArray.length ==1){
-// } 
-
-
-// for (var i=0; i<compChoice.length; i++){
-// 	if
-// }
-// if compChoice
-
-
-// document.onkeyup = function(event){
-// 	console.log ("user's choice: ", event.key);
-
-
-
-// }
 
 
 
